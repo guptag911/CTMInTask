eea785f88263e36228af271b19ec68e57c3ab395
abhi1381 <abhilnm011@gmail.com>
2020-05-29 22:37:23 +0530

    confluence api setup

--- functions/index.js
+++ functions/index.js
@@ -2,6 +2,19 @@
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 const cors = require("cors");
+const express = require("express");
+
+const app = express();
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "OPTIONS, GET, POST, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
 
 const serviceAccount = require("./serviceAccountKey.json");
 
@@ -12,6 +25,16 @@ admin.initializeApp({
 
 const db = admin.firestore();
 
+const authUrl =
+  // eslint-disable-next-line no-template-curly-in-string
+  "https://auth.atlassian.com/authorize?audience=api.atlassian.com&client_id=LcUQspyPyb8ATVkVEUN5KS4NuIxrI4mO&scope=read%3Aconfluence-content.summary%20read%3Aconfluence-space.summary%20read%3Aconfluence-props%20write%3Aconfluence-content%20read%3Aconfluence-content.all%20offline_access&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2F&state=${YOUR_USER_BOUND_VALUE}&response_type=code&prompt=consent";
+// Auth
+
+app.get("/auth", (req, res) => {
+  res.send(authUrl);
+});
+
+exports.api = functions.https.onRequest(app);
 // auth functions
 exports.newSignUp = functions.auth.user().onCreate(async (user) => {
   let userObject = {
@@ -22,32 +45,11 @@ exports.newSignUp = functions.auth.user().onCreate(async (user) => {
     uid: `${user.uid}`,
   };
 
-  return db.collection("users").doc(user.uid).set(userObject);
+  return db.collection("users").doc(user.uid).create(userObject);
 });
 
-exports.onUserDelete = functions.auth.user().onDelete(async (user) => {
+exports.onUserDelete = functions.auth.user().onDelete((user) => {
   const doc = db.collection("users").doc(user.uid);
+
   return doc.delete();
 });
-
-exports.syncData = functions.https.onCall(async (data, context) => {
-  if (context.auth) {
-    console.log(context.auth.uid);
-    let ref = await db.collection(`users/${context.auth.uid}/tasks`).get();
-    console.log(ref.docs);
-    return ref.docs;
-    // // eslint-disable-next-line promise/catch-or-return
-    // sfRef.listCollections().then((collections) => {
-    //   collections.forEach((collection) => {
-    //     return collection;
-    //   });
-    // });
-  }
-});
-
-// exports.scheduledFunction = functions.pubsub
-//   .schedule("every 5 minutes")
-//   .onRun((context) => {
-//     console.log("This will be run every 5 minutes!");
-//     return null;
-//   });
