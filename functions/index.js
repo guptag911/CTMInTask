c870fcb8b4263c967de6b3577eb16b1557119473
bittukumarray <raybittu242@gmail.com>
2020-06-01 16:02:54 +0530

    practiced firebase function

--- functions/index.js
+++ functions/index.js
@@ -61,3 +61,68 @@ exports.onUserDelete = functions.auth.user().onDelete((user) => {
 
   return doc.delete();
 });
+
+
+// /**
+//  * Google Cloud Function that responds to messages sent from a
+//  * Hangouts Chat room.
+//  *
+//  * @param {Object} req Request sent from Hangouts Chat room
+//  * @param {Object} res Response to send back
+//  */
+exports.helloHangoutsChat = functions.https.onRequest((req, res)=> {
+  if (req.method === 'GET' || !req.body.message) {
+    res.send('Hello! This function is meant to be used in a Hangouts Chat ' +
+      'Room.');
+  }
+
+  const sender = req.body.message.sender.displayName;
+  const image = req.body.message.sender.avatarUrl;
+
+  const data = createMessage(sender, image);
+
+  res.send(data);
+});
+
+// /**
+//  * Creates a card with two widgets.
+//  * @param {string} displayName the sender's display name
+//  * @param {string} imageURL the URL for the sender's avatar
+//  * @return {Object} a card with the user's avatar.
+//  */
+function createMessage(displayName, imageURL) {
+  const cardHeader = {
+    'title': 'Hello ' + displayName + '!',
+  };
+
+  const avatarWidget = {
+    'textParagraph': {'text': 'Your avatar picture: '},
+  };
+
+  const avatarImageWidget = {
+    image: {'imageUrl': imageURL},
+  };
+
+  const avatarSection = {
+    'widgets': [
+      avatarWidget,
+      avatarImageWidget,
+    ],
+  };
+
+  return {
+    'cards': [{
+      'name': 'Avatar Card',
+      'header': cardHeader,
+      'sections': [avatarSection],
+    }],
+  };
+}
+
+
+
+
+exports.addMessage = functions.https.onRequest(async (req, res) => {
+  
+  res.json({result: `Message with ID: added.`});
+});
