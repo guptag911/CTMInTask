a5e274250807b7aa89a1a751c84103cc26a16b45
guptag911 <17ucc003@lnmiit.ac.in>
2020-06-05 12:51:46 +0530

    Reply Functions Added

--- src/api/gsuiteApi.js
+++ src/api/gsuiteApi.js
@@ -67,3 +67,62 @@ export const GsuiteGetId = async () => {
     return [];
   }
 };
+
+export const GsuiteDataSaveReply = async (tid, userdata) => {
+    try {
+      // console.log("CURRENT USER IS ", firebaseAuth.currentUser.uid);
+      // console.log("In gsuite funct");
+      userdata["upload_time_utc"] = Date.now();
+      const uid = firebaseAuth.currentUser.uid;
+      const userRef = await db
+        .collection("users")
+        .doc(uid)
+        .collection("tasks")
+        .doc("gsuite")
+        .collection("reply")
+        .doc(tid)
+        .set(userdata);
+      // console.log("userRef is ", userRef);
+      return { msg: "success" };
+    } catch (e) {
+      console.log("error is ", e);
+      return { msg: "fail" };
+    }
+  };
+
+  export const GsuiteDataGetReply = async () => {
+    try {
+      const uid = firebaseAuth.currentUser.uid;
+      const userRef = await db
+        .collection("users")
+        .doc(uid)
+        .collection("tasks")
+        .doc("gsuite")
+        .collection("reply")
+        .get();
+      var finalData = [];
+      userRef.forEach((data) => {
+        finalData.push(data.data());
+      });
+      // console.log("Data is ", finalData);
+      return finalData;
+    } catch (e) {
+      console.log("error is ", e);
+      return [];
+    }
+  };
+
+  export const GsuiteGetIdreply = async () => {
+    try {
+      var my_data = GsuiteDataGetReply();
+      var ids = [];
+      (await my_data).forEach((data) => {
+        ids.push(data["tid"]);
+      });
+      //console.log("ids are->", ids)
+      return ids;
+    } catch (e) {
+      console.log("Error is", e);
+      return [];
+    }
+  };
