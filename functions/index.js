668725fb8132e2e2790368530ef5709b9ef416ba
abhi1381 <abhilnm011@gmail.com>
2020-06-06 19:33:18 +0530

    hub auth

--- functions/index.js
+++ functions/index.js
@@ -1,10 +1,32 @@
 /* eslint-disable promise/always-return */
+const request = require("request-promise");
+const NodeCache = require("node-cache");
+const session = require("express-session");
+const bodyParser = require("body-parser");
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 const cors = require("cors");
 const express = require("express");
 
 const app = express();
+
+const refreshTokenStore = {};
+const accessTokenCache = new NodeCache({ deleteOnExpire: true });
+
+const serviceAccount = require("./serviceAccountKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://ctmintask.firebaseio.com",
+});
+
+app.use(
+  session({
+    secret: Math.random().toString(36).substring(2),
+    resave: false,
+    saveUninitialized: true,
+  })
+);
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
@@ -16,23 +38,31 @@ app.use((req, res, next) => {
   next();
 });
 
-const serviceAccount = require("./serviceAccountKey.json");
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://ctmintask.firebaseio.com",
-});
+app.use(bodyParser.json());
 
 const db = admin.firestore();
 
 const authUrl =
   // eslint-disable-next-line no-template-curly-in-string
-  "https://auth.atlassian.com/authorize?audience=api.atlassian.com&client_id=LcUQspyPyb8ATVkVEUN5KS4NuIxrI4mO&scope=read%3Aconfluence-content.summary%20read%3Aconfluence-space.summary%20read%3Aconfluence-props%20write%3Aconfluence-content%20read%3Aconfluence-content.all%20offline_access&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2F&state=${YOUR_USER_BOUND_VALUE}&response_type=code&prompt=consent";
+  "https://auth.atlassian.com/authorize?audience=api.atlassian.com&client_id=LcUQspyPyb8ATVkVEUN5KS4NuIxrI4mO&scope=read%3Aconfluence-content.summary%20read%3Aconfluence-space.summary%20read%3Aconfluence-props%20write%3Aconfluence-content%20read%3Aconfluence-content.all%20offline_access&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2F%23%2Fdash&state=${YOUR_USER_BOUND_VALUE}&response_type=code&prompt=consent";
 // Auth
 
 // Uer Auth
 const userAuth =
-  "https://auth.atlassian.com/authorize?audience=api.atlassian.com&client_id=LcUQspyPyb8ATVkVEUN5KS4NuIxrI4mO&scope=read%3Ame%20offline_access&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2F&state=${YOUR_USER_BOUND_VALUE}&response_type=code&prompt=consent";
+  "https://auth.atlassian.com/authorize?audience=api.atlassian.com&client_id=LcUQspyPyb8ATVkVEUN5KS4NuIxrI4mO&scope=read%3Ame%20offline_access&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2F%23%2Fdash&state=${YOUR_USER_BOUND_VALUE}&response_type=code&prompt=consent";
+
+// Hubspot
+const hubAuth =
+  "https://app.hubspot.com/oauth/authorize?client_id=49a97a69-1406-4a1d-8eb3-64b9cbed6126&scope=contacts%20sales-email-read&redirect_uri=http://localhost:3000/";
+
+const jiraAuth =
+  "https://auth.atlassian.com/authorize?audience=api.atlassian.com&client_id=LcUQspyPyb8ATVkVEUN5KS4NuIxrI4mO&scope=read%3Ajira-user%20read%3Ajira-work%20write%3Ajira-work%20offline_access&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2F%23%2Fdash&state=${YOUR_USER_BOUND_VALUE}&response_type=code&prompt=consent";
 
 app.get("/auth", (req, res) => {
   res.send(authUrl);
@@ -40,6 +70,44 @@ app.get("/auth", (req, res) => {
 
 app.get("/user", (req, res) => {
   res.send(userAuth);
+});
+
+app.get("/hub", (req, res) => {
+  res.send(hubAuth);
+});
+
+app.get("/jira", (req, res) => {
+  res.send(jiraAuth);
+});
+
+app.post("/code", async (req, res) => {
+  const authCode = req.body.authCode;
+  const config = {
+    grant_type: "authorization_code",
+    client_id: "49a97a69-1406-4a1d-8eb3-64b9cbed6126",
+    client_secret: "0840ac22-bc6d-49dc-90f0-06b472924e17",
+    redirect_uri: "http://localhost:3000/",
+    code: `${authCode}`,
+  };
+
+  const options = {
+    method: "POST",
+    uri: "https://api.hubapi.com/oauth/v1/token",
+    body: config,
+    json: true,
+  };
+
+  const responseBody = await request(options);
+
+  const tokens = JSON.parse(responseBody);
+  refreshTokenStore[req.sessionID] = tokens.refresh_token;
+  accessTokenCache.set(
+    req.sessionID,
+    tokens.access_token,
+    Math.round(tokens.expires_in * 0.75)
+  );
+
+  res.send(tokens.access_token);
 });
 
 exports.api = functions.https.onRequest(app);
