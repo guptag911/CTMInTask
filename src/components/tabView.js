8b57e0031311ad9c1f3b40c1f77c5f8bd6736954
abhi1381 <abhilnm011@gmail.com>
2020-06-07 00:02:20 +0530

    hubspot fix

--- src/components/tabView.js
+++ src/components/tabView.js
@@ -83,9 +83,6 @@ export default function ScrollableTabsButtonAuto() {
     JSON.parse(localStorage.getItem("jira"))
   );
 
-  const [hub, setHub] = React.useState(false);
-  const [conf, setConf] = React.useState(false);
-  const [Jira, setJira] = React.useState(false);
   const [clickState, setclickState] = React.useState(
     JSON.parse(localStorage.getItem("state")) || {
       hub: false,
@@ -141,6 +138,7 @@ export default function ScrollableTabsButtonAuto() {
     localStorage.setItem("state", JSON.stringify(state));
 
     const res = await hubAuth();
+    console.log(res);
     window.location.href = res;
   };
 
@@ -166,10 +164,7 @@ export default function ScrollableTabsButtonAuto() {
   }, [window.onload]);
 
   const handleAuth = async () => {
-    if (
-      authState &&
-      !window.localStorage.getItem("user")
-    ) {
+    if (authState && !window.localStorage.getItem("user")) {
       await getUserToken();
     } else {
       await getToken();
