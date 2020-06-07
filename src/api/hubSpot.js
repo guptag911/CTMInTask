01158624f00cef0be297f30a6925c77622c9c26d
abhi1381 <abhilnm011@gmail.com>
2020-06-07 18:35:18 +0530

    ui integ pending

--- src/api/hubSpot.js
+++ src/api/hubSpot.js
@@ -28,7 +28,7 @@ export const HubSpotTasksGet = async () => {
   try {
     const contactId = await GetContactId();
     // console.log("contact id is ", contactId);
-    if (contactId[0] != -1) {
+    if (contactId[0] !== -1) {
       const HubToken = await hub.getHubToken();
       const result = await axios.get(
         proxyurl +
--- src/api/jira.js
+++ src/api/jira.js
@@ -44,26 +44,26 @@ async function user() {
   }
 }
 
-async function subtasks_data(subtasks_list) {
-  let subtasks = []; //list of subtasks
-  try {
-    subtasks_list.forEach(async (element) => {
-      let sub_schema = {}; //schema of subtasks
-      sub_schema["subtask_id"] = element.id;
-      let fields_list = element.fields;
-      sub_schema["subtask_name"] = fields_list.summary;                                             
-      //sub_schema['due_date'] = fields_list.duedate;
-      sub_schema["status"] = fields_list.status.name;
-      sub_schema["priority"] = fields_list.priority.name;
-      //sub_schema['assigned_by'] = fields_list.creator.displayName;
-      console.log(sub_schema);
-      subtasks.push(sub_schema);
-    });
-    return subtasks;
-  } catch (err) {
-    console.log("Error is:", err);
-  }
-}
+// async function subtasks_data(subtasks_list) {
+//   let subtasks = []; //list of subtasks
+//   try {
+//     subtasks_list.forEach(async (element) => {
+//       let sub_schema = {}; //schema of subtasks
+//       sub_schema["subtask_id"] = element.id;
+//       let fields_list = element.fields;
+//       sub_schema["subtask_name"] = fields_list.summary;
+//       //sub_schema['due_date'] = fields_list.duedate;
+//       sub_schema["status"] = fields_list.status.name;
+//       sub_schema["priority"] = fields_list.priority.name;
+//       //sub_schema['assigned_by'] = fields_list.creator.displayName;
+//       console.log(sub_schema);
+//       subtasks.push(sub_schema);
+//     });
+//     return subtasks;
+//   } catch (err) {
+//     console.log("Error is:", err);
+//   }
+// }
 
 async function issues_data() {
   let user_schema = {};
@@ -71,7 +71,7 @@ async function issues_data() {
     let account_ID = await user();
     console.log(account_ID);
     let issues_list = await issues(account_ID);
-    //console.log(issues_list);
+    console.log(issues_list);
     issues_list
       ? issues_list.forEach(async (element) => {
           let issue_ID = element.id;
@@ -81,12 +81,13 @@ async function issues_data() {
           let db_ids = await get_JiraID();
           if (db_ids.includes(issue_ID)) {
             //change the status accordingly
+
             try {
               const uid =
                 firebaseAuth.currentUser.uid === null
                   ? JSON.parse(window.sessionStorage.getItem("user")).uid
                   : firebaseAuth.currentUser.uid;
-
+              console.log(uid);
               const useref = await db
                 .collection("users")
                 .doc(uid)
@@ -111,10 +112,12 @@ async function issues_data() {
             user_schema["due_date"] = fields_list.duedate;
             user_schema["status"] = status;
             user_schema["priority"] = fields_list.priority.name;
-            let subtasks_list = fields_list.subtasks;
-            if (subtasks_list !== null)
-              user_schema["subtasks"] = await subtasks_data(subtasks_list);
-            else user_schema["subtasks"] = null;
+            // let subtasks_list = fields_list.subtasks;
+            // if (subtasks_list !== null) {
+            //   user_schema["subtasks"] = await subtasks_data(subtasks_list);
+            // } else {
+            //   user_schema["subtasks"] = null;
+            // }
             console.log(user_schema);
             //saving the data
             let db_data = await save_JiraData(
