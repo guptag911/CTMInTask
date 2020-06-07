af5da4345984dc84bb2776720868841e09f1fac6
anvy2803 <anvesha2803@gmail.com>
2020-06-07 17:00:06 +0530

    jira connected

--- src/api/jira.js
+++ src/api/jira.js
@@ -6,6 +6,12 @@ import {
     firebaseConfig,
     db,
   } from "../config/config";
+import {
+  get_JiraID,
+  get_JiraData,
+  save_JiraData,
+}
+from "./atlassian";
 
 async function issues(account_ID) {
     try{
@@ -45,21 +51,22 @@ async function user() {
   }
 
 async function subtasks_data (subtasks_list) {
-    let subtask = [];
+    let subtasks = [];    //list of subtasks
     try
     {
       subtasks_list.forEach(async (element) => {
-      let sub_schema = {}; 
-      sub_schema['subtask_ID'] = element.id;
+      let sub_schema = {};    //schema of subtasks
+      sub_schema['subtask_id'] = element.id;
       let fields_list = element.fields;
       sub_schema['subtask_name'] = fields_list.summary;
-      sub_schema['due_date'] = fields_list.duedate;
+      //sub_schema['due_date'] = fields_list.duedate;
       sub_schema['status'] = fields_list.status.name;
       sub_schema['priority'] = fields_list.priority.name;
-      sub_schema['assigned_by'] = fields_list.creator.displayName;
-      subtask.push(sub_schema);
+      //sub_schema['assigned_by'] = fields_list.creator.displayName;
+      console.log(sub_schema);
+      subtasks.push(sub_schema);
       });
-      return subtask;      
+      return subtasks;      
     }
     catch(err)
     {
@@ -68,15 +75,46 @@ async function subtasks_data (subtasks_list) {
 } 
 
 async function issues_data () {
-    let account_ID = await user();
-    //console.log(account_ID);
-    let issues_list = await issues(account_ID);
-    //console.log(issues_list);
     let user_schema = {};
     try
     {
-    issues_list.forEach(async (element) => {
+    let account_ID = await user();
+    console.log(account_ID);
+    let issues_list = await issues(account_ID);
+    //console.log(issues_list);
+    issues_list
+    ? issues_list.forEach(async (element) => {
         let issue_ID = element.id;
+        let status = fields_list.status.name;
+        //fetching issue ids from firestore
+        let db_ids = await get_JiraID();
+        if(db_ids.includes(issue_ID))
+        {
+          //change the status accordingly
+          try {
+            const uid =
+              firebaseAuth.currentUser.uid === null
+                ? JSON.parse(window.sessionStorage.getItem("user")).uid
+                : firebaseAuth.currentUser.uid;
+
+            const useref = await db
+              .collection("users")
+              .doc(uid)
+              .collection("tasks")
+              .doc("atlassian")
+              .collection("jira")
+              .doc(issue_ID)
+              .get();
+
+            let my_data = useref.data();
+            my_data["status"] = status;
+          } 
+          catch (err) {
+            console.log("error!", err);
+          }
+        }
+        else
+        {
         user_schema['issue_id'] = issue_ID;
         let fields_list = element.fields;
         user_schema['project_name'] = fields_list.project.name;
@@ -85,12 +123,19 @@ async function issues_data () {
         user_schema['url'] = url;
         user_schema['issue_name'] = fields_list.summary;
         user_schema['due_date'] = fields_list.duedate;
-        user_schema['status'] = fields_list.status.name;
+        user_schema['status'] = status;
         user_schema['priority'] = fields_list.priority.name;
-        user_schema['assigned_by'] = fields_list.creator.displayName;
         let subtasks_list = fields_list.subtasks;
-        //console.log(subtasks_list);
-        });
+        if(subtasks_list !== null)
+          user_schema['subtasks'] = await subtasks_data(subtasks_list);
+        else
+          user_schema['subtasks'] = null;
+        console.log(user_schema);
+        //saving the data
+        let db_data = await save_JiraData(user_schema["issue_id"],user_schema);
+        }
+      })
+    : null; 
     }
     catch(err)
     {
@@ -98,4 +143,9 @@ async function issues_data () {
     }
 }
 
-issues_data();
+if (
+  window.localStorage.getItem("user") &&
+  window.localStorage.getItem("jira")
+) {
+  issues_data();
+}
