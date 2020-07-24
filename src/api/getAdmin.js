e7337348c59abbaa318582cd145e23867bb463c0
bittukumarray <raybittu242@gmail.com>
2020-07-24 10:06:10 +0530

    some fixed

--- src/api/getAdmin.js
+++ src/api/getAdmin.js
@@ -2,11 +2,17 @@ import { firebaseAuth, db } from "../config/config";
 
 export const getIsAdmin = async () => {
   const uid =
-    firebaseAuth.currentUser.uid === null
+  firebaseAuth.currentUser === null || firebaseAuth.currentUser.uid === null
       ? JSON.parse(window.sessionStorage.getItem("user")).uid
       : firebaseAuth.currentUser.uid;
 
+  try{
   const result = await db.collection("users").doc(uid).get();
   console.log(result.data().isAdmin);
   return result.data().isAdmin;
+  }
+  catch(e){
+    console.log("error is ", e);
+    return false;
+  }
 };
